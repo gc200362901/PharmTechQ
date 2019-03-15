76b3582f569c3b2f484b39e68a68841e47736273
Sean <gc200362901@gmail.com>
2019-03-14 23:21:56 -0400

    register user auth

--- app/src/main/java/appdevs/pharmtechq/RegisterActivity.java
+++ app/src/main/java/appdevs/pharmtechq/RegisterActivity.java
@@ -1,14 +1,19 @@
 package appdevs.pharmtechq;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -18,23 +23,55 @@ public class RegisterActivity extends AppCompatActivity {
     EditText editTextPassword;
     EditText edittextConfirmPassword;
     Button buttonRegister;
-    //ProgressBar
+    ProgressBar progressBarRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        authDb = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        edittextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+        progressBarRegister = findViewById(R.id.progressBarRegister);
+        progressBarRegister.setVisibility(View.INVISIBLE);
     }
 
     public void registerUser(View view) {
-        //validates
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String confirmPassword = edittextConfirmPassword.getText().toString();
 
-        //saves the values into database online
+        if(TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter an email", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        progressBarRegister.setVisibility(View.VISIBLE);
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-
+        authDb.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void loginActivity(View view) {
