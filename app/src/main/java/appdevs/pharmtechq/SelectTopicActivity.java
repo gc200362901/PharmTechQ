37eab85bd45a6afb6f7ba6c4ebdd3eaa7e1d447b
Sean <gc200362901@gmail.com>
2019-03-16 12:01:50 -0400

    actionbar menu

--- app/src/main/java/appdevs/pharmtechq/SelectTopicActivity.java
+++ app/src/main/java/appdevs/pharmtechq/SelectTopicActivity.java
@@ -1,11 +1,16 @@
 package appdevs.pharmtechq;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,18 +25,51 @@ import appdevs.pharmtechq.models.Question;
 
 public class SelectTopicActivity extends AppCompatActivity {
 
+    Toolbar toolBar;
     private HashSet<String> tmpTopics;
     ArrayList<String> topics;
     private DatabaseReference db;
+    FirebaseAuth authDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_topic);
 
+        authDb = FirebaseAuth.getInstance();
+        toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+
         populateTopics();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_select_topics:
+                return true;
+            case R.id.action_profile:
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                return true;
+            case R.id.action_logout:
+                authDb.signOut();
+                //finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Connect to Firebase database to question.topic. Add topics to hashset to remove duplicates
+     * and then convert hashset to arraylist to be used in recyclerview
+     */
     private void populateTopics() {
         
         db = FirebaseDatabase.getInstance().getReference("questions");
@@ -46,7 +84,6 @@ public class SelectTopicActivity extends AppCompatActivity {
                     tmpTopics.add(question.getQuestionTopic());
                 }
 
-                //convert topics hashset into arraylist for recyclerview
                 topics = new ArrayList<>(tmpTopics);
 
                 initRecyclerView();
@@ -59,6 +96,12 @@ public class SelectTopicActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Get reference to recyclerview. Instantiate the adapter for the recyclerview with the
+     * context and topics arraylist arguments. Set the adapter for the recyclerview. Set the
+     * layout manager for the recyclerview to linear layout manager so recyclerview loads vertical
+     * list
+     */
     private void initRecyclerView() {
         
         RecyclerView recyclerView = findViewById(R.id.recyclerViewTopics);
